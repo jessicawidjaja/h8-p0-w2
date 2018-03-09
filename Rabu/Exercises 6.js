87ac35e40328faf65659057a87b24045ab812f0a
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-09 20:25:15 +0700

    Update Exercises 6.js

--- Rabu/Exercises 6.js
+++ Rabu/Exercises 6.js
@@ -1,61 +1,28 @@
 
 1. Melakukan Looping Menggunakan While
+var i = 0;
 
-var coding = 0;
-
-	console.log('LOOPING PERTAMA')
-while(coding < 20) { 
-  
-	coding += 2; 
- 
- 	console.log(coding + ' - I love coding');
-}
+console.log('LOOPING PERTAMA');
+while(i < 20) {
+  i += 2;
+  console.log(i + '- I Love Coding'); }
 
 
-var developer = 22;
-
-	console.log('LOOPING KEDUA')
-while(developer > 2){
- 
-	developer -= 2;
- 
-	console.log(developer + ' I will become fullstack developer');
-}
-
+console.log('LOOPING KEDUA');
+while(i > 0){
+  console.log(i + "- I will become fullstack developer");
+  i -= 2; }
 ---------------------------------------------------------------------------------------------------
 
 2. Melakukan Looping Menggunakan For
 
-var coding = 0;
+console.log('LOOPING PERTAMA');
+for (var a=1; a <= 20; a++) {
+  console.log(a + " - I Love coding"); }
 
-
-	console.log('LOOPING PERTAMA')
-
-for(var a=1;a<= 20; a++) { 
-  
-	
-	coding += 1; 
- 
- 	
-	console.log(coding + ' - I love coding');
-
-}
-
-
-
-var developer = 21;
-
-
-	console.log('LOOPING KEDUA')
-
-for(var a=20; a>=1; a--){
- 
-	
-	developer -= 1;
- 
-	
-	console.log(developer + ' I will become fullstack developer');
-}
+console.log('LOOPING KEDUA');
+for (var a=20; a >= 1; a--){
+  console.log(a + ' - I will become fullstack developer'); }
 
 ------------------------------------------------------------------------------------------------------
 
@@ -63,20 +30,14 @@ for(var a=20; a>=1; a--){
 -------- 1. GANJIL GENAP
 
 var a;
-
 for (a=1; a<=100; a++)
-{
- 
-if (a % 2 === 0){
-   
+{ 
+if (a % 2 === 0){   
 console.log(a + ' - GENAP')
-
 } 
-else{
-    
+else{   
 console.log(a + ' - GANJIL')
 }
-
 }
 
 -------- 2. KELIPATAN 3
