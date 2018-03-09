b222b32cff623733396a02c19f20ac4f4fdb2720
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-09 21:24:55 +0700

    Update Exercises 7.js

--- Rabu/Exercises 7.js
+++ Rabu/Exercises 7.js
@@ -14,48 +14,23 @@ console.log('*');
 2. Menyusun Barisan Bintang Dengan Nested Looping
 
 
-var rows2 ;
-
-
-for (rows2 = 1; rows2 <= 3; rows2++) 
-{
-console.log('***');
-
-for (rows2 = 1; rows2 <= 2; rows2++) 
-{
-console.log('***');
-
-for (rows2 = 1; rows2 <= 1; rows2++) 
-{
-console.log('***'); 
+var str = '';
+  for (var a=1; a<=3; a++) {
+  for (var b=1; b<=3; b++) {
+    str += '*';
 }
- } 
+  str += '\n';
 }
-
+console.log(str)
 
 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
 
 
-var rows3 ;
-
-
-
-for (rows3 = 1; rows3 <= 3; rows3++) 
-
-{
-console.log('*');
-
-
-for (rows3 = 1; rows3 <= 2; rows3++) 
-
-{
-console.log('**');
-
-
-for (rows3 = 1; rows3 <= 1; rows3++) 
-
-{
-console.log('***'); 
+var str = '';
+  for (var a=1; a<=3; a++) {
+  for (var b=1; b<=a; b++) {
+    str += '*';
 }
- } 
+  str += '\n';
 }
+console.log(str)
